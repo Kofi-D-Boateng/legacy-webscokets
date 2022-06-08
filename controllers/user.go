eb9bf8c87ef4afcca59a658f50661126b219beaf
Kofi Boateng <kdboat2@gmail.com>
2022-06-07 20:02:14 -0500

    Successfully implemented mongodb and fetching of users.

--- controllers/user.go
+++ controllers/user.go
@@ -1,28 +1,47 @@
 package controllers
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
-	"os"
 
 	"github.com/Kofi-D-Boateng/legacynotifications/database"
+	"github.com/Kofi-D-Boateng/legacynotifications/models"
 )
 
-var usersCollection string = os.Getenv("USERS_COLLECTION")
 
 func GetNotificationsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	email := r.URL.Query().Get("email")
-	go database.Find(email)
-
-
-
-
+	var user models.User
+	err := models.Decoder.Decode(&user, r.URL.Query())
+	if err != nil {
+		log.Fatal(err)
+	}
+	foundUser := database.FindAUser(user.Email)
+	json.NewEncoder(w).Encode(foundUser)
 }
 
 func SetNotificationsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var transaction models.Transaction
+	err := models.Decoder.Decode(&transaction, r.URL.Query())
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result bool = database.InsertUserAndNotification(transaction)
+	json.NewEncoder(w).Encode(result)
 }
 
 func MarkNotificationsHandler(w http.ResponseWriter, r *http.Request) {
+	var variables models.MarkMessage
+	err := models.Decoder.Decode(&variables, r.URL.Query())
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result bool = database.MarkMessageAsRead(variables)
+	json.NewEncoder(w).Encode(result)
 }
--- models/models.go
+++ models/models.go
@@ -1,9 +1,41 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/gorilla/schema"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var Decoder = schema.NewDecoder()
+
 
 type User struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Email         string `json:"email,omitempty"`
-	Notifications []string `json:"notifications,omitempty"`
+	Email         string `json:"email,omitempty" bson:"email,omitempty"`
+	Notifications []bson.M `json:"notifications,omitempty" bson:"notifications,omitempty"`
+}
+
+type Transaction struct {
+	ID            		primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	AccountNumber 		string `json:"accountNumber" bson:"accountNumber,omitempty"`
+	CardNumber 			uint64 `json:"cardNumber" bson:"cardNumber,omitempty"`
+	Cvc 				uint64 `json:"cvc" bson:"cvc,omitempty"`
+	EmailOfTransferee 	string `json:"emailOfTransferee" bson:"emailOfTransferee,omitempty"`
+	DateOfTransaction 	primitive.DateTime `json:"dateOfTransaction" bson:"dateOfTransaction,omitempty"`
+	Type 				string `json:"type" bson:"type,omitempty"`
+	Amount 				float64 `json:"amount" bson:"amount,omitempty"`
+	PhoneNumber 		string `json:"phoneNumber" bson:"phoneNumber,omitempty"`
+}
+
+type MarkMessage struct {
+	Email 		string `json:"email"`
+	MsgID 		string `json:"msg_id"`
+}
+
+type CustomerServiceMessage struct {
+	ID            		primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Emailer 			string `json:"emailer,omitempty" bson:"emailer,omitempty"`
+	Topic				string `json:"topic,omitempty" bson:"_id,omitempty"`
+	Message				string `json:"msg,omitempty" bson:"msg,omitempty"`
+	SentAt				string `json:"sentAt,omitempty" bson:"sentAt,omitempty"` 
 }
